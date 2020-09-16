207f4be4974f234c74861148e1889932826f0e8f
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2020-09-16 00:57:08 -0300

    (day01): created a hello world using cargo

---
+++ day01/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87.d
@@ -0,0 +1,5 @@
+/home/fabricio/projects/rust/rust-study/day01/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87: src/main.rs
+
+/home/fabricio/projects/rust/rust-study/day01/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87.d: src/main.rs
+
+src/main.rs:
---
+++ day01/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/fabricio/projects/rust/rust-study/day01/hello_cargo/target/debug/hello_cargo: /home/fabricio/projects/rust/rust-study/day01/hello_cargo/src/main.rs
